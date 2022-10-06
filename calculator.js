09088cfd77d34d75ff507a9eaf0c391b1e906df5
fredericgalline <frederic@galline.fr>
2022-10-06 16:45:27 +0200

    maj calculator

--- calculator.js
+++ calculator.js
@@ -1,25 +1,23 @@
 const userName = prompt("Quel est votre nom ?");
 const random = Math.floor(Math.random() * 100) + 1;
 let number = parseInt(prompt("Choisir un nombre entre 1 et 100"));
-console.log("Random : " + random);
+//console.log("Random : " + random);
 let counter = 1;
 
 while (number != random) {
   if (number > 100) {
     console.log("On t'a dit entre 0 et 100 boulet");
   }
-
-  console.log("Number : " + number);
+  console.log("Essai " counter + ":" + number);
 
   if (number < random) {
-    const result = "C'est plus";
+    number = prompt("C'est plus, essaye encore");
   } else {
-    const result = "C'est moins";
+    number = prompt("C'est moins, essaye encore");
   }
 
-  //const result = number < random ? "C'est plus" : "C'est moins";
-  number = prompt(result + ", Essaye encore");
   counter++;
 }
 
 console.log(userName + ", t'as gagné en " + counter + " coups");
+alert(userName + ", t'as gagné en " + counter + " coups");
